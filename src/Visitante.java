d75a9d58f9edbb8001d6e19b5ebf681390bdeeb0
irvin <irving.gutierrez@uip.edu.pa>
2024-03-15 16:19:20 -0500

    visita 2

---
+++ src/Visitante.java
@@ -0,0 +1,21 @@
+public class Visitante {
+
+        public static void main(String[] args) {
+
+            String nombre = "Irving";
+            int edad = 26;
+            char genero = 'M';
+            boolean esResidente = false;
+            double altura = 1.71;
+
+            // Impresión de la información del visitante en pantalla
+            System.out.println("Información del visitante:");
+            System.out.println("Nombre: " + nombre);
+            System.out.println("Edad: " + edad + " años");
+            System.out.println("Género: " + genero);
+            System.out.println("Es residente: " + esResidente);
+            System.out.println("Altura: " + altura + " metros");
+        }
+
+
+}
